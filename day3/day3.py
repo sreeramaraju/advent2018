727e1e0a153e8a9f89383fb60f695c5a0a974db3
sreeramaraju <sreeramaraju@gmail.com>
2018-12-03 07:43:02 -0800

    day3 setup

---
+++ day3/day3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def read_line(fname):
+    pass
+
+def dimentions(claims):
+    pass
+
+def mark_claims(claims, cloth):
+    pass
+
+def count_repeated_claims(cloth):
+    pass
+
+def main():
+    print("Welcome to Day3")
+
+if __name__ == '__main__':
+    main()
